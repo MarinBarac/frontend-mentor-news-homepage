1458afc040c53e55bdc0ef87281dee6e5fd10675
unknown <marinbarac21@gmail.com>
2022-12-22 14:21:12 +0100

    Added styles for mobile view

--- news-homepage/src/components/Layout/Header.js
+++ news-homepage/src/components/Layout/Header.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import logo from '../../assets/images/logo.svg';
 
 import styles from './Header.module.css';
@@ -5,16 +6,32 @@ import styles from './Header.module.css';
 const NAV_LINKS = ['Home', 'New', 'Popular', 'Trending', 'Categories'];
 
 const Header = () => {
- return <div className={styles.header}>
-    <div className={styles.logoContainer}>
-        <img src={logo} />
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu((prevShowMenu) => {
+            return !prevShowMenu;
+        });
+    }
+
+    console.log(showMenu);
+
+    return <div className={styles.header}>
+        <div className={`${styles.backdrop} ${showMenu ? styles.active : ''}`} />
+        <div className={styles.logoContainer}>
+            <img src={logo} />
+        </div>
+        <nav className={`${styles.navbar} ${showMenu ? styles.active : ''}`}>
+            <ul className={`${styles.navLinks}`}>
+                {NAV_LINKS.map(link => (<li className={styles.navLink}>{link}</li>))}
+            </ul>
+        </nav>
+        <div className={`${styles.hamburger} ${showMenu ? styles.active : ''}`} onClick={toggleMenu}>
+            <span className={`${styles.hamburgerBar} ${styles.barOne}`}/>
+            <span className={`${styles.hamburgerBar} ${styles.barTwo}`}/>
+            <span className={`${styles.hamburgerBar} ${styles.barThree}`}/>
+        </div>
     </div>
-    <nav className={styles.navbar}>
-        <ul className={styles.navLinks}>
-            {NAV_LINKS.map(link => (<li className={styles.navLink}>{link}</li>))}
-        </ul>
-    </nav>
- </div>
 }
 
 export default Header;
